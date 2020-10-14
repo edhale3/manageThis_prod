aaa14152cb3e65ad332feb0b0213b61a691ffeb6
edhale3 <edhale33@gmail.com>
2020-10-13 22:06:22 -0700

    Added comments

--- isLoggedIn.js
+++ isLoggedIn.js
@@ -1,5 +1,7 @@
 const passport = require('passport')
 
+//if user is logged in then continue the route, 
+//otherwise return to the front end that it needs authentication
 function isLoggedIn (req,res,next){
     if(req.user){
         next();
