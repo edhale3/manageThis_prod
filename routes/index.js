c54c06f4918fe1c2afa32391ebbfaf263c4fba9b
edhale3 <edhale33@gmail.com>
2020-10-16 21:19:01 -0700

    Added knew database setup

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,38 @@ const Account = require('../SQLQueries/Account')
 const passport = require('passport');
 const bcrypt = require('bcrypt')
 const isLoggedIn = require('../isLoggedIn');
+const db = require('knex')({
+    client:'pg',
+    connection: {
+        host:process.env.DB_HOST,
+        user:process.env.DB_USER,
+        password:process.env.DB_PW,
+        database: process.env.DB
+    },
+    pool: { min: 0, max:15 }
+})
+
+const deps = { db }
 
 let user = require('../controllers/user')
 let data = require('../controllers/data')
 
-//routing
-router.get('/api', user.home)
-router.get("/api/signup", user.signup) 
-router.post('/api/signup', user.postSignup)
-router.get('/api/signin', user.signin)
-router.post('/api/signin', passport.authenticate('local'), user.postSignin)
-router.get('/api/logout', user.logout)
-router.get("/api/account", isLoggedIn, user.account)
+router.get('/api', user.signin)//
+router.get("/api/signup", user.signup) //
+router.post('/api/signup', user.postSignup(deps))//
+router.get('/api/signin', user.signin)//
+router.post('/api/signin', passport.authenticate('local'), user.postSignin)//
+router.get('/api/logout', user.logout)//
+router.get("/api/account", isLoggedIn , user.account(deps))//
 router.get('/api/newproject', isLoggedIn, (req,res)=> {
     console.log("You got here now")
 })  
-router.post('/api/newproject', isLoggedIn, data.postProject )
-router.get("/api/projects", isLoggedIn, data.projects)
-router.post("/api/createcomment", isLoggedIn, data.postComment)
-router.get("/api/getcomments/:project_id", isLoggedIn, data.getComments)
-router.patch("/api/updateproject/:project_id", isLoggedIn, data.updateProject)
-router.delete('/api/deleteproject/:project_id', isLoggedIn, data.deleteProject)
-router.delete('/api/deletecomment/:comment_id', isLoggedIn, data.deleteComment)
-
+router.post('/api/newproject', isLoggedIn, data.postProject(deps))//
+router.get("/api/projects", isLoggedIn, data.projects(deps))//
+router.post("/api/createcomment", isLoggedIn, data.postComment(deps))//
+router.get("/api/getcomments/:project_id", isLoggedIn, data.getComments(deps))//
+router.patch("/api/updateproject/:project_id", isLoggedIn, data.updateProject(deps))
+router.delete('/api/deleteproject/:project_id', isLoggedIn, data.deleteProject(deps))
+router.delete('/api/deletecomment/:comment_id', isLoggedIn, data.deleteComment(deps))
 
 module.exports = router;
