d8077d8dd4897ec7c99b13f802d108115daf2761
edhale3 <edhale33@gmail.com>
2020-10-18 17:10:27 -0700

    Added more custom error handlers

--- controllers/data.js
+++ controllers/data.js
@@ -1,110 +1,64 @@
 const express = require('express')
 const passport = require('passport');
 const bcrypt = require('bcrypt')
-
-function failure(code, message, res){
-  return res.status(code).json({error: message})
-}
-
-exports.projects = (deps) => {
-  const db = deps.db
-  return async (req, res) => {
-    try {
-      const records = await db('projects').where({account_id:req.user[0].id})
-      return res.json(records)
-    } catch (err) {
-      console.log('An error occurred looking up the projects: ', err)
-      return failure(500, 'Internal Error', res)
-    }
-  }
-}
-
+const PostingError = require('../errors/PostingError');
+const ReferenceError = require('../errors/ResourceNotFoundError');
+const ResourceNotFoundError = require('../errors/ResourceNotFoundError');
+//function for posting a project
 exports.postProject = (deps) => {
   const db = deps.db
   return async (req, res) => {
-    try {
       const post = await db('projects').insert({title: req.body.title,project_status: req.body.project_status,
         description: req.body.description,account_id: req.user[0].id 
-      })
+      }).catch( err => { throw new PostingError('projects', req.body, err.message)})
       return res.send({message: "Congratulations your project was created"})
-    } catch (err) {
-      console.log("Your project was not posted")
-      return failure(500, "Your project was not posted", res)
-    }
   }
 }
-
 //function for posting comment as the signed in user
 exports.postComment = (deps) => {
   const db = deps.db
   return async (req, res)=> {
-    try {
       const data = await db('comments').insert({comment_title:req.body.comment_title, 
         comment_body:req.body.comment_body, project_id:req.body.project_id
-      })
-      return res.send("Comment created")
-    } catch (err){
-      console.log("this is the err: ", err)
-      return failure(500, 'Your comment could not be posted', res)
-    }
+      }).catch( err => { throw new PostingError('comments', req.body, err.message)})
+      return res.send({message:"Comment created"})
   }
 }
-
 //function for getting all comments associated with the signed in user
 exports.getComments = (deps) => {
   const db = deps.db
   return async (req, res) => {
-    try {
       const data = await db('comments').select().where({project_id: req.params.project_id})
-      res.send(data)
-    } catch (err){
-      console.log("This is the error for the get comments: ", err)
-      return failure(500, "Your comments could not be retrieved", res)
-    }
+      .catch( err => { console.log('this error: ', err); throw new ResourceNotFoundError('comments', req.params.project_id)})
+      return res.send(data)
   }
 }
-
+//function for updating project details
 exports.updateProject = (deps) => {
   const db = deps.db
   return async (req, res) => {
-    try {
       const update = await db('projects')
-      .update({title:req.body.title, 
-              project_status:req.body.project_status,
-              description: req.body.description
-      })
+      .update({title:req.body.title, project_status:req.body.project_status, description: req.body.description})
       .where({project_id: req.body.project_id})
-      return res.send("The project was updated")
-    } catch (err){
-      console.log("This is the updateProject error: ", err)
-      return failure(500, "You project could not be updated", res)
-    }
+      .catch( err => { throw new PostingError('comments', req.body, err.message)})
+      return res.send({message:"The project was updated"})
   }
 }
-
+//function for deleting entire project
 exports.deleteProject = (deps) => {
   const db = deps.db
   return async (req, res) => {
-    try{
       const deleteProject = await db('projects').del().where({project_id:req.params.project_id})
-      console.log(deleteProject)
+      .catch( err => {throw new Error(err)})
       return res.send("Success")
-    } catch (err){
-      console.log("This is the delete project error: ", err)
-      return failure(500, "Your project was not deleted", res)
-    }
   }
 }
-
+//function for deleting comment
 exports.deleteComment = (deps) => {
     const db = deps.db
     return async (req, res) => {
-      try {
         const deleteComment = await db('comments').del().where({comment_id: req.params.comment_id})
+        .catch( err => {throw new Error(err)})
         return res.send("Success")
-      } catch (err) {
-        console.log("Your comment was not deleted")
-        return fauilure(500, "Your comment was not deleted", res)
-      }
     }
 }
--- controllers/user.js
+++ controllers/user.js
@@ -2,34 +2,18 @@ const express = require('express')
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 const ResourceNotFoundError = require('../errors/ResourceNotFoundError');
-// const database = require('../services/db')
-// const { Pool, Client } = require('pg');
-// //create pool of clients with connection string to connect to the 'manage' database
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL
-// });
+const PostingError = require('../errors/PostingError');
 
-function failure(code, message, res){
-  return res.status(code).json({error: message})
-}
 //function calls logout function that will end the session
 exports.logout = (req, res) => {
   req.logout()
   res.redirect("/")
 }
 
-//check if signed in before rendering sign in page
-exports.signin = (req, res) => {
-  res.send(req.isAuthenticated())
-}
-
 //if authentication was successful this function will set up the cookie settings
 exports.postSignin = (req, res) => {
-    if (req.body.remember) {
-      req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000
-    } else {
-      req.session.cookie.expires = false;
-    }
+    if (req.body.remember) {req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000
+    } else {req.session.cookie.expires = false;}
     return res.send(req.user)
 }
 
@@ -46,30 +30,23 @@ exports.account = (deps) => {
       return res.json(records) 
   }
 }
-
-//get signup route (doesn't do anything right now)
-exports.signup = (req,res) => {
-    console.log("isAuthenticated: ", req.isAuthenticated())
-}
-
 //post signup data and create new account (checks if the account already exists)
 exports.postSignup = (deps) => {
   const db = deps.db
   return async function (req, res){
-    try {
-      const records = await db('accounts').select('account_id').where('email',`${req.body.email}`)
+      const records = await db('accounts')
+      .select('account_id')
+      .where('email',`${req.body.email}`)
       if(records[0]){
-        return failure(404, 'This user exists. Navigate to sign in page')
-      } else {
-        let pwd = await bcrypt.hash(req.body.password, 8);
-        const signup = await db('accounts').insert({first_name:req.body.first_name,
-          last_name:req.body.last_name,email:req.body.email,gender:req.body.gender,password:pwd
-        })
-        return res.send("Congrats it worked")
+        throw new PostingError('accounts', req.body.email , `An account with the email '${req.body.email}' already exists.`)
       }
-    } catch (err) {
-      console.log("Your signup process failed", err)
-      return failure(500, 'Your signup process failed', res)
-    }
+      let pwd = await bcrypt.hash(req.body.password, 8);
+      const signup = await db('accounts')
+      .insert({first_name:req.body.first_name, last_name:req.body.last_name,email:req.body.email,gender:req.body.gender,password:pwd
+      })
+      .catch(err => {
+        throw new PostingError('accounts', req.body, err.message)
+      })
+      return res.send("Congrats it worked")
   }
 }
